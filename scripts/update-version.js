d9e23ee3dded05cf019f1c9c9b624c3ccfe35a6f
Devjosef <joseffsdev@gmail.com>
2025-08-07 11:58:58 +0200

    Add build scripts

---
+++ scripts/update-version.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const pkg = require('../package.json');
+const content = `export const VERSION = '${pkg.version}';\n`;
+fs.writeFileSync('src/types/version.ts', content); 
