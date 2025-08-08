ca1d72478f11c58a0957192c8f183904eb5918f2
Devjosef <joseffsdev@gmail.com>
2025-08-08 07:37:01 +0200

    Build: fix Vite config to preserve HTML navigation links

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ export default defineConfig({
   root: 'documents',
   build: {
     outDir: '../dist/docs',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: 'documents/index.html',
+        'getting-started': 'documents/getting-started.html',
+        rules: 'documents/rules.html',
+        docs: 'documents/docs.html',
+        performance: 'documents/performance.html'
+      }
+    }
   },
   server: {
     port: 3000
